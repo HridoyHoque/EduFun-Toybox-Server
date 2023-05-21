7ad32e4c5d3d2a6972ceb58917116b0de8e1ef3a
HridoyHoque <hhridoy155@gmail.com>
2023-05-21 06:44:39 +0600

    send delete data request

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -54,6 +54,15 @@ async function run() {
       const result = await toyCollection.insertOne(newToy);
       res.send(result);
     })
+
+    // delete My Toys Data 
+    app.delete('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCollection.deleteOne(query);
+      res.send(result);
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
